3073c32465975ae0fafee8bba4b6e62ab93d5c8e
TinVo13 <votrungtinoppo@gmail.com>
2023-07-15 02:30:32 +0700

    hoan thanh dang nhap

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  checkoutForm = this.formBuilder.group({
+    email: '',
+    password: '',
+  });
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+  onSubmit(): void {
+    // Process checkout data here
+    if (this.checkoutForm.invalid) {
+      alert('Email hoac mat khau khong trung khop!'); 
+      this.checkoutForm.reset();
+    }
+    else {
+      console.warn('Your order has been submitted', this.checkoutForm.value);
+      this.checkoutForm.reset();
+    }
+  }
 }
